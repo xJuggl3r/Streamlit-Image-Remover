2c1413c3bc3c910b4ac464573e2b64041ffcc10f
xjuggl3r <xjuggl3r@gmail.com>
2023-02-08 18:56:38 -0400

    first commit

---
+++ bg_remove.py
@@ -0,0 +1,52 @@
+from io import BytesIO
+
+import streamlit as st
+from PIL import Image
+from rembg import remove
+
+st.set_page_config(layout="wide", page_title="Image Background Remover")
+
+st.write("## Git rid of your image's background!")
+st.write(
+    ":dog: Upload an image to watch the background magically get removed. Full quality images can be downloaded from the sidebar. Yay to the [rembg library](<https://github.com/danielgatis/rembg>) :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
+
+# Create the columns
+col1, col2 = st.columns(2)
+
+# Download the fixed image
+
+
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
+# Package the transform into a function
+
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button(
+        "Download fixed image", convert_image(fixed), "fixed.png", "image/png"
+    )
+
+
+# Create the file uploader
+my_upload = st.sidebar.file_uploader(
+    "Upload an image", type=["png", "jpg", "jpeg"])
+
+# Fix the image!
+if my_upload is not None:
+    fix_image(upload=my_upload)
+else:
+    fix_image("./spaceman.png")
